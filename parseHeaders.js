56355756c509997d3b4971aaf225aae261a4dff5
Edward <edpantzar@gmail.com>
2018-07-30 12:50:54 -0500

    All tests passing

--- parseHeaders.js
+++ parseHeaders.js
@@ -3,8 +3,36 @@
 /* eslint-env node */
 
 function parseHeaders(text) {
-    // your code here
-    return text;
+    let obj = {};
+    obj.headers = {};
+    obj.body;
+    let arr = text.split('\n');
+    let count = 0;
+    while (arr.length > 0) {
+        if (count === 0) {
+        let lineOneArr = arr[0].split(' ');
+        obj.version = lineOneArr[0];
+        obj.code = lineOneArr[1];
+        arr.shift();
+        count++;
+        } 
+        else {
+        let keyValueArr = arr[0].split(': ');
+        if (keyValueArr[0].length === 0){
+            break;
+        } 
+        else {
+            obj.headers[keyValueArr[0]] = keyValueArr[1];
+            arr.shift();
+        }
+        }
+    }
+    obj.body = arr.join(' ');
+    arr.shift();
+    console.log(obj);
+    return obj;
 }
+      
+
 
 module.exports = parseHeaders;
